a9c6521080db2184f07bbdf259b1ab011d1b1c3d
dochner <dochner@users.noreply.github.com>
2022-07-31 17:54:59 +0000

    deploy: 215418ae6a22381237c5ec1213c8a51e7a38575d

---
+++ _nuxt/default-39d483d7.mjs
@@ -0,0 +1,1 @@
+import{_ as a}from"./plugin-vue_export-helper-84c1d017.mjs";import{o as n,a as o,b as e,r}from"./entry-34ad9a68.mjs";const t={},s=e("header",null,null,-1),c=e("aside",null,null,-1);function _(l,d){return n(),o("div",null,[s,c,e("main",null,[r(l.$slots,"default")])])}var i=a(t,[["render",_]]);export{i as default};
