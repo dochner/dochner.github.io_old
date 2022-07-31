2c422de1b61d2049eeae1edeb961e360c68b70b0
dochner <dochner@users.noreply.github.com>
2022-07-31 18:13:23 +0000

    deploy: 25c6a883d46e8836ba293bdee8adbeea5766ba2e

--- _nuxt/index-cc06e5a8.mjs
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./plugin-vue_export-helper-84c1d017.mjs";import{o as r,a as o}from"./entry-34ad9a68.mjs";const c={};function n(t,a){return r(),o("div",null," Hello World! ")}var s=e(c,[["render",n]]);export{s as default};
